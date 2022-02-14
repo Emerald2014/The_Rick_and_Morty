1fddc2cc097d2a48c2c42e7702562dfb610bf434
Alex <emerald2014@mail.ru>
2022-02-14 18:36:23 +0300

    Initial commit

---
+++ app/src/main/java/ru/kudesnik/therickandmorty/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.kudesnik.therickandmorty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.kudesnik.therickandmorty.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/kudesnik/therickandmorty/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.kudesnik.therickandmorty.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
